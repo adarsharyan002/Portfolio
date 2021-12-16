6fa085629aefab14d74168422a25d0a9c9661672
Northy <adarsharyanmuz@gmail.com>
2021-12-16 14:00:26 +0530

    update

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,11 +4,11 @@ const About = () => {
     return (
         <div className="Container-about" id="About">
           <h1 class="Title">About Me</h1>
-          <p>I am a passionate Full Stack Developer who loves to explore new technologies and build scalable applications.Although I expertize in full-stack development, I prefer to work on the backend of any application.I am also interested in Blockchain development using Solidity, and can create blockchain-based applications.I have a good hold over developing Decentralized Applications or DApps using solidity smart-contracts.
+          <p>I am a passionate Full Stack Developer who loves to explore new technologies and build scalable applications.Although I expertize in full-stack development, I prefer to work on the forntend of any application.I have a good hold over developing Decentralized Applications or DApps using solidity smart-contracts.
 
-I am also highly into Open Source and like to contribute to different projects. Currently, I have made 90+ Pull Requests, and created 30+ Issues on GitHub.More information about my Open Source profile is available at GitHub | Kailash Kejriwal.
+I am also highly into Open Source and like to contribute to different projects. 
 
-Apart from coding, I love to write technical blogs on the latest and emerging technologies.All of my blogs are available on Medium | Kailash Kejriwal, where I post articles frequently.</p>
+.</p>
           
           
         </div>
