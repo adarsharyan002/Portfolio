cb22d235a32c51e642e5452e8a225d4cb6dd0c6a
Northy <adarsharyanmuz@gmail.com>
2021-12-29 19:34:44 +0530

    cleared-mistakes

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className="Container-about" id="About">
           <h1 class="Title">About Me</h1>
-          <p>I am a passionate Full Stack Developer who loves to explore new technologies and build scalable applications.Although I expertize in full-stack development, I prefer to work on the forntend of any application.
+          <p>I am a passionate Full Stack Developer who loves to explore new technologies and build scalable applications.Although I expertize in full-stack development, I prefer to work on the frontend of any application.
 
 I am also highly into Open Source and like to contribute to different projects. 
 
