c931f063d6a39f8732eb76d74db6810306c2c9fa
Northy <adarsharyanmuz@gmail.com>
2022-01-01 21:04:53 +0530

    changes

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,7 +6,6 @@ const About = () => {
           <h1 class="Title">About Me</h1>
           <p>I am a passionate Full Stack Developer who loves to explore new technologies and build scalable applications.Although I expertize in full-stack development, I prefer to work on the frontend of any application.
 
-I am also highly into Open Source and like to contribute to different projects. 
 
 .</p>
           
