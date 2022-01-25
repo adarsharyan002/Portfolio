f1b710e082e38779878933cc33b833cf3b2b0aa0
Northy <adarsharyanmuz@gmail.com>
2022-01-25 11:09:36 +0530

    Adding project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,6 @@
  import React_blog from '../../assets/React_blog.png'
  import Chat_application from '../../assets/Chat_application.png'
+ import Movie from '../../assets/Movie.png'
  
  
  const Projects =[
@@ -17,6 +18,13 @@
         Description:"It's a real time chat application made using socket.io",
         code:"https://github.com/adarsharyan002/Chat-app-socket.io",
         url:"https://youthful-noyce-d8c620.netlify.app/"
+    },
+    {
+        Name:"Movie-Booking App",
+        image:Chat_application,
+        Description:"It is a movie booking website made uisng TMDB Api",
+        code:"https://github.com/adarsharyan002/Movie-booking-Front-end",
+        url:"https://ecstatic-almeida-df2774.netlify.app/"
     }
 
 ]
